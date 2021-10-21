f2d9f769ecf413f20f78ad9ddd546989f87bfca8
stkirk <shaanekirk@gmail.com>
2021-10-21 17:11:57 -0400

    recipe router setup

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const helmet = require("helmet");
+const recipeRouter = require("./recipe-router");
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
-//router here
+
+server.use("/api/recipes", recipeRouter);
 
 server.get("/api", (req, res) => {
   res.send("You found recipes api, use /api/recipes for requests");
